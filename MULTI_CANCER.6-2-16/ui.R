97e7e49295e52fd4c25394031840d9baa1f4c87d
Kevin P Keegan <kevin@wireless-s1-no-150-17-61.uchicago.edu>
2016-06-03 16:42:03 -0500

    added 4 cancers and aml examples

---
+++ MULTI_CANCER.6-2-16/ui.R
@@ -0,0 +1,26 @@
+library(shiny)
+library(shinyRGL)
+
+# Define UI for application that draws a histogram
+shinyUI(fluidPage(
+
+# Application title
+    titlePanel("Expression Profiles for Four Types of Cancer"),
+
+    # Sidebar with a slider input for the number of bins
+    sidebarLayout(position="right",
+                  sidebarPanel(
+                      selectInput("colorby", "Color By:",
+                                  choices=c("Cancer_Type" = "Cancer_Type"
+                                            )),
+                      plotOutput("distPlot")
+                      
+                  ),
+
+                  # Show a plot of the generated distribution
+                  mainPanel(
+                  # webGLOutput("myWebGL", width=800, height=800)
+                      webGLOutput("myWebGL", width=1200, height=1200)
+                  )
+                  )
+))
